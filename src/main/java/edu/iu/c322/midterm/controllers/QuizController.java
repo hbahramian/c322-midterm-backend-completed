0fc525a39d60cc14aeeb4b6d1a529c43c3be8c78
hbahramian <habahram@iu.edu>
2024-03-12 13:19:06 -0700

    Initial commit

---
+++ src/main/java/edu/iu/c322/midterm/controllers/QuizController.java
@@ -0,0 +1,9 @@
+package edu.iu.c322.midterm.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quizzes")
+public class QuizController {
+}
