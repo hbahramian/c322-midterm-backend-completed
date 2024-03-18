4d37fb0d7e62544d6e3f6ab1ebfaee6505994f65
hbahramian <habahram@iu.edu>
2024-03-17 23:15:09 -0700

    .

--- src/main/java/edu/iu/c322/midterm/controllers/QuizController.java
+++ src/main/java/edu/iu/c322/midterm/controllers/QuizController.java
@@ -6,6 +6,7 @@ import edu.iu.c322.midterm.repository.FileRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -23,6 +24,15 @@ public class QuizController {
     public int add(@RequestBody Quiz quiz) {
         try {
             return fileRepository.add(quiz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping
+    public List<Quiz> findAll() {
+        try {
+            return fileRepository.findAllQuizzes();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
